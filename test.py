27457e0b7f2a05c8e0cfcf11aa8494a6c28c98ef
unknown <taktuk07@gmail.com>
2026-02-06 14:50:16 +0200

    a

--- test.py
+++ test.py
@@ -5,7 +5,11 @@ led = PWMLED(17)
 while True:
     led.value = 0  # off
     sleep(1)
+    led.value = 0.2
+    sleep(1)
     led.value = 0.5  # half brightness
+    sleep(1)
+    led.value = 0.7  # half brightness
     sleep(1)
     led.value = 1  # full brightness
     sleep(1)
