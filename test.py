471adc8a82d350e4c8a7c7a52fb60de1cf9e59ff
unknown <taktuk07@gmail.com>
2026-02-06 15:02:20 +0200

    a

--- test.py
+++ test.py
@@ -2,22 +2,22 @@ from gpiozero import PWMLED
 from time import sleep
 led = PWMLED(17)
 
-# while True:
-#     led.value = 0  # off
-#     sleep(1)
-#     led.value = 0.2
-#     sleep(1)
-#     led.value = 0.5  # half brightness
-#     sleep(1)
-#     led.value = 0.7  # half brightness
-#     sleep(1)
-#     led.value = 1  # full brightness
-#     sleep(1)
-counter = 0
 while True:
-    led.value = counter
-    print(led.value)  # off
-    sleep(0.001)
-    counter += 0.1
-    if counter == 1:
-        counter = 0
+    led.value = 0  # off
+    sleep(1)
+    led.value = 0.2
+    sleep(1)
+    led.value = 0.5  # half brightness
+    sleep(1)
+    led.value = 0.7  # half brightness
+    sleep(1)
+    led.value = 1  # full brightness
+    sleep(1)
+# counter = 0
+# while True:
+#     led.value = counter
+#     print(led.value)  # off
+#     sleep(0.001)
+#     counter += 0.1
+#     if counter == 1:
+#         counter = 0
