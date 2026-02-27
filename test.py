e4ac451279eb284b3c6f08511c80dda6bcb3795e
unknown <taktuk07@gmail.com>
2026-02-27 16:45:46 +0200

    s

--- test.py
+++ test.py
@@ -18,35 +18,53 @@ import datetime
 from asyncio import sleep
 
 
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Pull-Up כי פולס = GND
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 count = 0
-befor = datetime.datetime.now()
+last_pulse_time = None
+
 def pulse_detected(channel):
-        global befor
-        global count
-        count += 1
-        after = datetime.datetime.now()
-        if after - befor > datetime.datetime(year=0,month=0,hour=0, minute=00,second=00 ,microsecond=0.00014):
-             print('aa')
-             
-        # a = after - befor
-        # print(a)
-        
-       
+    global count
+    global last_pulse_time
+
+    count += 1
+    last_pulse_time = datetime.datetime.now()
 
 def count_coin():
-    global befor
-    befor = datetime.datetime.now()
-    GPIO.add_event_detect(17, GPIO.FALLING, callback=pulse_detected, bouncetime=50)
+    global count
+    global last_pulse_time
+
+    GPIO.add_event_detect(17, GPIO.FALLING, callback=pulse_detected, bouncetime=5)
+
     try:
         while True:
-            sleep(1)
+            time.sleep(0.05)
+
+            if last_pulse_time is not None:
+                delta = datetime.datetime.now() - last_pulse_time
+
+                # אם עברו 200 מילישניות בלי פולס חדש
+                if delta > datetime.timedelta(milliseconds=200):
+
+                    print("Finished pulses:", count)
+
+                    if count == 10:
+                        print("5 Shekel detected")
+                    elif count == 15:
+                        print("10 Shekel detected")
+                    else:
+                        print("Unknown coin")
+
+                    # איפוס לסבב הבא
+                    count = 0
+                    last_pulse_time = None
+
     except KeyboardInterrupt:
         GPIO.cleanup()
 
-count_coin()        
+count_coin()
 
 
 # reader = SimpleMFRC522()
